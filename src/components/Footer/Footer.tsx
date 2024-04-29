cad4814ecd2c584d6be7ec3129348549e0293af8
rgsk <guptarahul70322@gmail.com>
2024-04-29 17:21:53 +0530

    social icons

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,5 +1,24 @@
+/* eslint-disable @next/next/no-img-element */
 import Link from "next/link";
 import Container from "../Shared/Container/Container";
+import TargetBlankLink from "../Shared/TargetBlankLink/TargetBlankLink";
+type SocialDetail = {
+  imageSrc: string;
+  link: string;
+  alt: string;
+};
+const socialDetails: SocialDetail[] = [
+  {
+    imageSrc: "/X.jpg",
+    link: "https://twitter.com/_skartner",
+    alt: "X logo",
+  },
+  {
+    imageSrc: "/linkedin.png",
+    link: "https://www.linkedin.com/company/skartner",
+    alt: "linkedin logo",
+  },
+];
 
 interface FooterProps {}
 const Footer: React.FC<FooterProps> = ({}) => {
@@ -8,13 +27,21 @@ const Footer: React.FC<FooterProps> = ({}) => {
       <div className="bg-gray-100">
         <Container>
           <footer className="py-[10px] md:py-[30px] ">
-            <div className="flex flex-col gap-2 items-start justify-between md:flex-row-reverse">
+            <div className="flex flex-col gap-2 items-start justify-between md:flex-row-reverse md:items-center">
               <p className="sm:text-xl">
                 Contact Me -&nbsp;
                 <a href="mailto:rahulguptasde@gmail.com">
                   rahulguptasde@gmail.com
                 </a>
               </p>
+              <div className="flex items-center gap-2 md:gap-4">
+                <p className="sm:text-xl">Follow Us:</p>
+                <div className="flex gap-2 md:gap-4">
+                  {socialDetails.map((detail, i) => {
+                    return <SocialLink key={i} detail={detail} />;
+                  })}
+                </div>
+              </div>
               <div className="flex items-center gap-2">
                 <Link href="/privacy-policy" className="sm:text-xl">
                   Privacy Policy
@@ -32,3 +59,18 @@ const Footer: React.FC<FooterProps> = ({}) => {
   );
 };
 export default Footer;
+
+interface SocialLinkProps {
+  detail: SocialDetail;
+}
+const SocialLink: React.FC<SocialLinkProps> = ({ detail }) => {
+  return (
+    <TargetBlankLink href={detail.link}>
+      <img
+        src={detail.imageSrc}
+        alt={detail.alt}
+        className="w-[30px] h-[30px] md:w-[40px] md:h-[40px] rounded-full"
+      />
+    </TargetBlankLink>
+  );
+};
